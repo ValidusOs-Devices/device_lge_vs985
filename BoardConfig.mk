crdroidandroid/android_device_lge_d855

bt: add vnd_g3.txt
Change-Id: Iea6f65a46db23ec1eadcbd903a708f11c68f6db6

BoardConfig.mk
@@ -24,6 +24,7 @@ TARGET_OTA_ASSERT_DEVICE := g3,d855
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/d855/bluetooth
+BOARD_CUSTOM_BT_CONFIG := device/lge/d855/bluetooth/vnd_g3.txt
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_d855_defconfig

ValidusOs-Devices/device_lge_vs985

BoardConfig.mk
@@ -27,6 +27,7 @@ TARGET_BOARD_INFO_FILE ?= device/lge/vs985/board-info.txt
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/vs985/bluetooth
+BOARD_CUSTOM_BT_CONFIG := device/lge/vs985/bluetooth/vnd_g3.txt
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_vs985_defconfig